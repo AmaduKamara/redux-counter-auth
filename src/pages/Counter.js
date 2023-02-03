5faa9b29c934d383f53aa32f08d2d8aad9fdadba
Amkam <amaduskamara@gmail.com>
2023-02-03 23:45:13 +0000

    Update Counter.js

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -3,9 +3,12 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
-  const toggleCounter = () => {};
+  const toggleCounter = () => {
+    dispatch({ type: "toggle" });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -30,42 +33,47 @@ const Counter = () => {
           Redux Counter App
         </h1>
         <hr className="my-3" />
-        <div>
-          <h2 className="text-5xl text-center font-bold">{counter}</h2>
-        </div>
-        <hr className="my-3" />
-        <div className="my-4 flex justify-center items-center">
-          <div className="flex items-center space-x-6">
-            <button
-              type="button"
-              className="bg-cyan-500 text-white py-3 px-6 rounded-md shadow-xl cursor-pointer"
-              onClick={incrementHandler}
-            >
-              Increment
-            </button>
-            <button
-              type="button"
-              className="bg-cyan-500 text-white py-3 px-6 rounded-md shadow-xl cursor-pointer"
-              onClick={decrementHandler}
-            >
-              Decrement
-            </button>
-            <button
-              type="button"
-              className="bg-cyan-500 text-white py-3 px-6 rounded-md shadow-xl cursor-pointer"
-              onClick={increaseHandler}
-            >
-              Increase by 5
-            </button>
-            <button
-              type="button"
-              className="bg-cyan-500 text-white py-3 px-6 rounded-md shadow-xl cursor-pointer"
-              onClick={descreaseHandler}
-            >
-              Decrease by 5
-            </button>
-          </div>
-        </div>
+        {show && (
+          <>
+            <div>
+              <h2 className="text-5xl text-center font-bold">{counter}</h2>
+            </div>
+            <hr className="my-3" />
+            <div className="my-4 flex justify-center items-center">
+              <div className="flex items-center space-x-6">
+                <button
+                  type="button"
+                  className="bg-cyan-500 text-white py-3 px-6 rounded-md shadow-xl cursor-pointer"
+                  onClick={incrementHandler}
+                >
+                  Increment
+                </button>
+                <button
+                  type="button"
+                  className="bg-cyan-500 text-white py-3 px-6 rounded-md shadow-xl cursor-pointer"
+                  onClick={decrementHandler}
+                >
+                  Decrement
+                </button>
+                <button
+                  type="button"
+                  className="bg-cyan-500 text-white py-3 px-6 rounded-md shadow-xl cursor-pointer"
+                  onClick={increaseHandler}
+                >
+                  Increase by 5
+                </button>
+                <button
+                  type="button"
+                  className="bg-cyan-500 text-white py-3 px-6 rounded-md shadow-xl cursor-pointer"
+                  onClick={descreaseHandler}
+                >
+                  Decrease by 5
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+
         <hr className="my-3" />
         <div className="my-4 flex justify-center items-center">
           <button

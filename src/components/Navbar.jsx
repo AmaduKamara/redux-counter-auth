e40ff03bb2cc4e3ff1aa2f76b8fbf35b57d01e0c
Amkam <amaduskamara@gmail.com>
2023-02-03 17:27:29 +0000

    Create and style navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="bg-white py-6 px-56 flex justify-between shadow-lg">
+      <Link to="/" className="text-xl">
+        Redux<em className="text-cyan-500 font-semibold">Counter</em>
+      </Link>
+
+      <nav>
+        <ul className="flex space-x-8">
+          <li className="cursor-pointer text-gray-600">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="cursor-pointer text-gray-600">
+            <Link to="/products">Products</Link>
+          </li>
+          <li className="cursor-pointer text-gray-600">
+            <Link
+              to="/login"
+              className="text-cyan-500 py-3 px-6 rounded-md border border-cyan-500 hover:bg-cyan-500 hover:text-white"
+            >
+              Login
+            </Link>
+          </li>
+          <li className="cursor-pointer text-gray-600">
+            <Link to="/" className="text-cyan-500 py-3 px-6 rounded-md border border-cyan-500 hover:bg-cyan-500 hover:text-white">
+              logout
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;

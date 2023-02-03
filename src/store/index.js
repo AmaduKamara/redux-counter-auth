9068298f8cb4a83a752b57b3e4d51dda18540818
Amkam <amaduskamara@gmail.com>
2023-02-03 23:26:25 +0000

    Create index.js

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.payload,
+    };
+  }
+
+  if (action.type === "decrease") {
+    return {
+      counter: state.counter - action.payload,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;

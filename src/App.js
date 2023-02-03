3a7e7ec0aa5044b5b4ba8fb076c79006de5de271
Amkam <amaduskamara@gmail.com>
2023-02-03 17:27:20 +0000

    Configure routing with components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Redux Counter App</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="py-16 px-56">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 

2b01ac69dd8de3f4cfcfefb38ce454cc89dc6549
Amkam <amaduskamara@gmail.com>
2023-02-05 08:30:28 +0000

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { authActions } from "../store/index";
+import { authActions } from "../store/authSlice";
 
 const Auth = () => {
   const dispatch = useDispatch();

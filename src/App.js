3c8f5ff2ed183170367667fcffe83e2757db92df
Amkam <amaduskamara@gmail.com>
2023-02-05 08:02:01 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Auth from "./components/Auth";
 import Counter from "./pages/Counter";
 import Products from "./pages/Products";
 
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <Navbar />
       <div className="py-16 px-56">
+        <Auth />
         <Routes>
           <Route path="/" element={<Counter />} />
           <Route path="/products" element={<Products />} />

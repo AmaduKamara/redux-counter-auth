2800285b7770c346ed46a2ce632e566b85f43d11
Amkam <amaduskamara@gmail.com>
2023-02-05 08:02:07 +0000

    Create Profile.jsx

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="flex justify-center items-center my-12">
+      <div className="w-[800px] shadow-xl bg-white p-6 rounded-md">
+        <h1 className="font-bold text-3xl text-cyan-500 text-center">
+          User Profile
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
